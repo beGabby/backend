23551a7f0ecfd6dfd4deed0696220451d492be80
Osiński Paweł <posinski@pentacomp.pl>
2021-03-11 23:41:38 +0100

    initliazie repo

---
+++ api/urls.py
@@ -0,0 +1,15 @@
+
+from django.urls import path
+#from .views import article_list, article_detailes
+from .views import ArticleList, ArticleDetails
+
+
+urlpatterns = [
+
+    path('articles', ArticleList.as_view()),
+    path('articles/<int:id>', ArticleDetails.as_view()),
+
+    #path('articles', article_list),
+    #path('articles/<int:pk>', article_detailes),
+
+]
