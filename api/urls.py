e87c4077c741bebdd4fa4ffe3c67a924211f9302
Osiński Paweł <posinski@pentacomp.pl>
2021-03-31 19:19:38 +0200

    repair versions, create module for testing

--- api/urls.py
+++ api/urls.py
@@ -5,8 +5,9 @@ from .views import ArticleViewSet, UserViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register('articles', ArticleViewSet, base_name='articles')
+router.register('articles', ArticleViewSet, basename='articles')
 router.register('users', UserViewSet)
+
 
 urlpatterns = [
 
